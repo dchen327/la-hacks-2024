5e219ff0297ff09b096c51a23f60fdef86dd517b
David Chen <davidc3287@gmail.com>
2024-04-19 23:21:14 -0700

    loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,33 @@
+"use client";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase/config";
+
 export default function Home() {
-  return (
-    <div>
-      <div className="button is-primary">Hello, world!</div>
-    </div>
-  );
+  const [user, loading, error] = useAuthState(auth);
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen mx-10">
+        <progress
+          className="progress is-small is-primary max-w-[500px]"
+          max="100"
+        ></progress>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p>Error: {error}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    return (
+      <div>
+        <div>Hello {user.displayName.split(" ")[0]}</div>
+      </div>
+    );
+  }
 }
