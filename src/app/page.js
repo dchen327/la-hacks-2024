827e1e7342343bc91c711a13e67c456db2f5bb28
David Chen <davidc3287@gmail.com>
2024-04-21 00:47:55 -0700

    title

--- src/app/page.js
+++ src/app/page.js
@@ -15,6 +15,8 @@ import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { geocodeByAddress, getLatLng } from "react-google-places-autocomplete";
 import { db } from "../app/firebase/config";
 import { collection, getDocs } from "firebase/firestore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
 
 const eventEmojis = {
   sport: "⚽",
@@ -170,6 +172,7 @@ export default function Home() {
       <div>
         <div className="title mx-2 mt-2 mb-0">
           Hello {user.displayName.split(" ")[0]}
+          <FontAwesomeIcon icon={faWind} size="sm" className="ml-3" />
         </div>
         <div>
           {/* <GooglePlacesAutocomplete
